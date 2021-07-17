b570efb32aa46dbd329fb8a2769cd5a41c356073
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-17 21:43:42 +0530

    [Vedhashni] Updated the file with Balancing Paranthesis

---
+++ DataStructurePrograms/BalancingParanthesis.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograms
+{
+    class BalancingParanthesis
+    {
+        public class StackOperations
+        {
+            char[] item;
+            int top = -1;
+            //Push method 
+            public bool Push(char x)
+            {
+                if (top == item.Length)
+                {
+                    return false;
+                }
+                else
+                {
+                    item[++top] = x;
+                    return true;
+                }
+            }
+            //Pop method
+            public char Pop()
+            {
+                if (top == -1)
+                {
+                    Console.WriteLine("Underflow error");
+                    return '\0';
+                }
+                else
+                {
+                    char element = item[top];
+                    top--;
+                    return element;
+                }
+            }
+            //To check the matching characters to pop from stack
+            public bool MatchingChar(char char1, char char2)
+            {
+                if (char1 == '[' && char2 == ']')
+                    return true;
+                else if (char1 == '{' && char2 == '}')
+                    return true;
+                else if (char1 == '(' && char2 == ')')
+                    return true;
+                else
+                    return false;
+            }
+            //Balance method to perform push and pop operation for opening and closing parantheses
+            public bool Isbalanced(char[] exp)
+            {
+                item = new char[exp.Length];
+                bool flag = true;
+                if (exp.Length == 0)
+                    return true;
+
+                for (int i = 0; i < exp.Length; i++)
+                {
+
+                    if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[')
+                    {
+                        Push(exp[i]);
+                        flag = false;
+                    }
+                    if (exp[i] == '}' || exp[i] == ']' || exp[i] == ')')
+                    {
+                        if (MatchingChar(Pop(), exp[i]))
+                            flag = true;
+                        else
+                            return false;
+                    }
+                }
+                return flag;
+            }
+        }
+    }
+}
--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -10,6 +10,27 @@ namespace DataStructurePrograms
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to DataStructure Programs");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    char[] expression = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)".ToCharArray();
+                    BalancingParanthesis.StackOperations stackOperations = new BalancingParanthesis.StackOperations();
+                    bool result = stackOperations.Isbalanced(expression);
+                    if (result)
+                    {
+                        Console.WriteLine("Parantheses Balanced");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parantheses not Balanced");
+                    }
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
